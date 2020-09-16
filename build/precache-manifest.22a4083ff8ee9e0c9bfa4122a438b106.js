92a43cc103a8ee152a89b2d1d5f6452f8e7f8510
Jevhenii <alexkorolixinofficial@gmail.com>
2020-09-16 18:24:16 +0300

    prod version

---
+++ build/precache-manifest.22a4083ff8ee9e0c9bfa4122a438b106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00619e01f4d7edb75b59d1a1d20765fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27c630df665f1c886b0",
+    "url": "/static/css/main.188edb03.chunk.css"
+  },
+  {
+    "revision": "ebf6b8dd43d1cce65003",
+    "url": "/static/js/2.c4936728.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4936728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27c630df665f1c886b0",
+    "url": "/static/js/main.f444c725.chunk.js"
+  },
+  {
+    "revision": "f94379dba18a6e483c43",
+    "url": "/static/js/runtime-main.f25682e7.js"
+  }
+]);
