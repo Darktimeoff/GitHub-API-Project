a710cfe916a1a9125961c37abd070592a50c5375
Jevhenii <alexkorolixinofficial@gmail.com>
2020-09-16 14:30:32 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { NavBar } from './components/Navbar';
+import { Home } from './pages/Home';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { About } from './pages/About';
+import { Profile } from './pages/Profile';
 
 
 function App() {
-  return (
-    <div className="container pt-4">
-      <h1>Hello World!</h1>
-    </div>
+	return (
+		<BrowserRouter>
+			<NavBar />
+			<div className="container pt-4">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+					<Route path="/profile/:name" component={Profile} />
+				</Switch>
+			</div>
+		</BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+export const NavBar = () => (
+    <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
+        <div className="navbar-brand">Github поиск</div>
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <NavLink to="/" className="nav-link" exact>Главная</NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink to="/about" className="nav-link">Информация</NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink to="/profile" className="nav-link">Профиль</NavLink>
+            </li>
+        </ul> 
+    </nav>
+)
